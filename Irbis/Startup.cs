6ba09426e8cd4f3e84c66688510b76591f29af5b
Betak6630 <Betak6630@mail.ru>
2017-03-16 19:30:30 +0300

    Cоздал MVC application

---
+++ Irbis/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Irbis.Startup))]
+namespace Irbis
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
