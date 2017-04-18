6311ff85d5e21d5c1ceed39fb1992027cabf4f88
Betak6630 <Betak6630@mail.ru>
2017-04-18 20:19:59 +0300

    Добавил dll Irbis.Core и Irbis.Entities

---
+++ Irbis.Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Irbis.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public ProductType Type { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ Irbis.Entities/ProductType.cs
@@ -0,0 +1,10 @@
+﻿namespace Irbis.Entities
+{
+    public class ProductType
+    {
+        public int Id { get;  set; }
+
+        public string Title { get;  set; }
+
+    }
+}
