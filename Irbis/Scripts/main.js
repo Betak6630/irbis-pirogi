51ad14e99aecab8e0eb06f5745d17574ad546afc
Betak6630 <Betak6630@mail.ru>
2017-05-21 19:48:16 +0300

    js код для корзины покупок

--- Irbis/Scripts/main.js
+++ Irbis/Scripts/main.js
@@ -1,8 +1,16 @@
 (function (irbis, $, undefined) {
     irbis.product = irbis.product || {};
 
-    irbis.product.addToCart = function () {
-        alert("Товар добавлен в корзину");
+    irbis.product.addToCart = function (productId) {
+        var product = $("#product-" + productId);
+        var optionProduct = $('input[name="option_' + productId + '"]:checked').val();
+        var countProduct = $("#product_" + productId + "_count").val();
+
+        console.info("Add product to ShoppingCart");
+        console.log("ProductId: " + productId);
+        console.log("OptionProduct: " + optionProduct);
+        console.log("CountProduct: " + countProduct);
+
     };
 
 
