597751a004b85927c6a84fb16b9e2487eaaa8160
Betak6630 <Betak6630@mail.ru>
2017-05-24 20:54:39 +0300

    add method GetShoppingСartByToken

--- Irbis.DataService/ShoppingСartDataService.cs
+++ Irbis.DataService/ShoppingСartDataService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Irbis.Entities;
 using PetaPoco;
 
@@ -25,6 +27,19 @@ namespace Irbis.DataService
 
             _db.Insert("ShoppingСart", "Id", shoppingCart);
 
+        }
+
+        public IEnumerable<ViewShoppingCart> GetShoppingСartByToken(Guid token)
+        {
+            string sqlQuery =
+                "select sc.ProductId, P.Name as ProductName, sc.ProductOptionId, po.Weight, p.ProductTypeId, sum(sc.Count) as Count, sum(sc.Count*po.Price) as TotalPrice " +
+                "from ShoppingСart sc" + "join Product p on sc.ProductId = p.Id " +
+                "join ProductOption po on po.Id = sc.ProductOptionId" + $" where Token = '{token}' " +
+                "group by sc.ProductId, P.Name, sc.ProductOptionId, po.Weight, p.ProductTypeId, po.Price";
+
+            var data = _db.Query<ViewShoppingCart>(sqlQuery).ToList();
+
+            return data;
         }
     }
 }
---
+++ Irbis.Entities/ViewShoppingCart.cs
@@ -0,0 +1,13 @@
+﻿namespace Irbis.Entities
+{
+    public class ViewShoppingCart
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int ProductOptionId { get; set; }
+        public int Weight { get; set; }
+        public int ProductTypeId { get; set; }
+        public int Count { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
