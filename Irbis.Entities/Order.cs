5f343ea7d596c405de3a055065ee868753163160
Betak6630 <Betak6630@mail.ru>
2017-06-09 19:21:53 +0300

    Добавил сущность Order в Irbis.Entities

---
+++ Irbis.Entities/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Irbis.Entities
+{
+    /// <summary>
+    /// Заказ
+    /// </summary>
+    public class Order
+    {
+        public int Id { get; set; }
+        public Guid Token { get; set; }
+        public int ProductId { get; set; }
+        public int ProductOptionId { get; set; }
+        public int Count { get; set; }
+        public string UserName { get; set; }
+        public string UserPhone { get; set; }
+        public string UserAddress { get; set; }
+        public string UserComment { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
