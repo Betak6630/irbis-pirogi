90aa9a256f654dd7b8704e1ec19e1c0b911f3a73
Betak6630 <Betak6630@mail.ru>
2017-06-10 12:29:05 +0300

    Добавил логику для метода SaveOrder

--- Irbis.DataService/OrderDataService.cs
+++ Irbis.DataService/OrderDataService.cs
@@ -17,7 +17,25 @@ namespace Irbis.DataService
 
         public void SaveOrder(IEnumerable<ShoppingСart> shoppingСarts, User user, Guid token)
         {
-            ;
+            var now = DateTime.Now;
+            var sqlInsert = "";
+
+            foreach (var shoppingСart in shoppingСarts)
+            {
+                sqlInsert += $"insert into [Order] Values('{shoppingСart.Token}', {shoppingСart.ProductId}, " +
+                                $"{shoppingСart.ProductOptionId}, {shoppingСart.Count}, '{user.Name}', '{user.Phone}'," +
+                                $"'{user.Address}', '{user.Comment}','{now}')\n";
+            }
+
+
+            using (var scope = _db.GetTransaction())
+            {
+                // Do transacted updates here
+                _db.Execute(sqlInsert);
+                // Commit
+                scope.Complete();
+
+            }
         }
     }
 }
