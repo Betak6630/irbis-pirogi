024ff98a72c2badac38c055d02605bd2a0a66100
Betak6630 <Betak6630@mail.ru>
2017-06-12 09:36:25 +0300

    Добавил контроллер Order

---
+++ Irbis/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Irbis.DataService;
+using Irbis.Models;
+using IndexViewModel = Irbis.Models.Order.IndexViewModel;
+
+namespace Irbis.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly OrderDataService _orderDataService;
+
+        public OrderController()
+        {
+            var dataService = new Irbis.DataService.Init();
+            _orderDataService = dataService.OrderDataService;
+        }
+
+        public ActionResult Index()
+        {
+            var viewModel = new IndexViewModel();
+
+            return View(viewModel);
+        }
+    }
+}
