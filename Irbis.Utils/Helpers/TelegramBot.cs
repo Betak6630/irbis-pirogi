2cc244f1db4d8c791a90c47f3c8e0a00aea85763
Betak6630 <Betak6630@mail.ru>
2017-06-13 19:50:52 +0300

    Add Helpers TelegramBot

---
+++ Irbis.Utils/Helpers/TelegramBot.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using RestSharp;
+
+namespace Irbis.Utils.Helpers
+{
+    public static class TelegramBot
+    {
+        public static void SendMessageOrder(string text)
+        {
+            var message = new StringBuilder();
+
+            try
+            {
+                var client = new RestClient("https://api.telegram.org");
+                client.Execute(RequestBuilder(Groups.Order, message.ToString()));
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+        private static RestRequest RequestBuilder(int telegramGroup, string message)
+        {
+            var request = new RestRequest("/bot393884541:AAHxqTR03mbphMB5PK0YTMi3rBo6ObpegGw/sendMessage",
+                Method.GET);
+
+            request.AddParameter("chat_id", telegramGroup);
+            request.AddParameter("disable_web_page_preview", true);
+            request.AddParameter("text", message);
+            request.AddParameter("parse_mode", "Markdown");
+            return request;
+        }
+
+        public static class Groups
+        {
+            public const int Order = -219554415;
+        }
+
+    }
+}
